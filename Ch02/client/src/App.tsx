22beb9dab2869205260e87a219298eb996c09d75
Kathleen Ly <heykathl@users.noreply.github.com>
2022-06-27 21:48:34 +0100

    Played around with types of data structures

--- Ch02/client/src/App.tsx
+++ Ch02/client/src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { stringify } from 'querystring';
 
 function App() {
+
+  // let firstValue: string = "kathleen";
+  // let firstValue: number = 21;
+  // let firstValue: boolean = true;
+  // let firstValue: number[] = [1,2,3];
+  let firstValue: Array<string> = ['hey', 'bye'];
+
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          The value {firstValue} is of {typeof firstValue} type!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
