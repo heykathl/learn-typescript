c6e6cf5a9e56f60c636451dccc5bfd430fa82b56
Kathleen Ly <heykathl@users.noreply.github.com>
2022-06-28 21:50:06 +0100

    Learned to leverage methods inside components

--- Ch02/client/src/App.tsx
+++ Ch02/client/src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Message from './Message'
 
-function App() {
+class App extends Component<any> {
+  componentWillMount() {
+    // Call this to ensure it is executed prior to rendering
+    console.log('Almost there...')
+  }
+
+  componentDidMount() {
+    console.log('hello')
+  }
 
   // let firstValue: string = "kathleen";
   // let firstValue: number = 21;
@@ -12,28 +20,31 @@ function App() {
   // let firstValue: Array<string> = ['hey', 'bye'];
 
   // Tuple - an array which can contain multiple types
-  let aTuple: [string, number] = ["Kathleen", 10];
+  // let aTuple: [string, number] = ["Kathleen", 10];
 
   // Enum - Similar to objects, stores multiple and can be initialized inside
-  enum Codes {first = 1, second = 2};
+  // enum Codes {first = 1, second = 2};
 
   // Any - unsure what type of data
-  let firstName: any = 10;
+  // let firstName: any = 10;
 
   // Void - Void a return type or function
-  const warning = (): void => {
-    console.log('Warning');
+  // const warning = (): void => {
+  //   console.log('Warning');
+  // }
+
+  render() {
+    return (
+  
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Message message='This is a simple message'/>
+        </header>
+      </div>
+    );
   }
-
-  return (
-
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Message message='This is a simple message'/>
-      </header>
-    </div>
-  );
+  
 }
 
 export default App;
